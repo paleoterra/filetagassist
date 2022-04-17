6dc6bcf3ad030aa98de562aa13e1b823d7adfbe9
Thomas Moore <sundown@mac.com>
2022-04-16 19:56:50 -0500

    missed a public

--- Sources/FileTagAssist/Extensions/String+Extension.swift
+++ Sources/FileTagAssist/Extensions/String+Extension.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension String {
-    func tags(separator: String = ",") -> [Tag] {
+    public func tags(separator: String = ",") -> [Tag] {
         let names = self.components(separatedBy: separator)
         return names.map { name in
             Tag(id: name.trimmingCharacters(in: .whitespacesAndNewlines))
